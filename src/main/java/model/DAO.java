375286c461043787b0e0c5a9cb9110f1f42b95f5
mberth03 <mberth03@cst-tp-b10111>
2019-03-26 13:35:44 +0100

    Ajout fonction DAO : Liste des commandes client

--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -5,7 +5,11 @@
  */
 package model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 import javax.sql.DataSource;
 
@@ -24,6 +28,17 @@ public class DAO {
         this.myDataSource=dataSource;
     }
     public List<PurchaseOrder> commandesClient (Customer c) throws SQLException{
-        
+        List<PurchaseOrder> resultat=new LinkedList<>();
+        String sql="SELECT ORDER_NUM, CUSTOMER_ID, PRODUCT_ID, QUANTITY, SHIPPING_COST, SALES_DATE, SHIPPING_DATE, PRODUCT.DESCRIPTION FROM CUSTOMER INNER JOIN PURCHASE_ORDER ON CUSTOMER.ID=PURCHASE_ORDER.CUSTOMER_ID WHERE CUSTOMER_ID=?";
+        try (Connection connection=myDataSource.getConnection();
+             PreparedStatement stmt=connection.prepareStatement(sql)) {
+                ResultSet rs=stmt.executeQuery();
+                while(rs.next()){
+                    int numComm=rs.getInt("ORDER_NUM");
+                    int numClient=rs.getInt("CUSTOMER_ID");
+                    int numProduit=rs.getInt("PRODUCT_ID");
+                    
+                }
+        }
     }
 }
