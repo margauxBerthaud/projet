be423fef27f9eff52df3c73c6cbbba3580ad2ef8
nvollhes <nvollhes@cst-tp-b10112>
2019-04-05 10:00:43 +0200

    Création class customerController, pas finie reste les requetes d'ajout d'edition et de suppression de commande/produits

--- src/main/java/controller/AdministratorController.java
+++ src/main/java/controller/AdministratorController.java
@@ -6,10 +6,7 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,7 +16,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.ws.rs.core.Link;
 import model.DAO;
 
 /**
